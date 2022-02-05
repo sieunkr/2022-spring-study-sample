7530cd360974dbc70887539bd44daa27ec1b4f35
Eddy.Kim <sieunkr@gmail.com>
2022-02-05 10:13:32 +0900

    ...

--- src/main/java/com/example/demo/SimpleMovieRepository.java
+++ src/main/java/com/example/demo/SimpleMovieRepository.java
@@ -22,8 +22,8 @@ public class SimpleMovieRepository implements MovieRepository {
     public List<Movie> findByTitle(String title) {
 
         HttpHeaders httpHeaders = new HttpHeaders();
-        httpHeaders.add("X-Naver-Client-Id", "RnUST_xBNVvRRuGbiDkq");
-        httpHeaders.add("X-Naver-Client-Secret", "eI5wkgmJ84");
+        httpHeaders.add("X-Naver-Client-Id", "-");
+        httpHeaders.add("X-Naver-Client-Secret", "-");
         String url = "https://openapi.naver.com/v1/search/movie.json" + "?query=" + title;
         return restTemplate.exchange(url, HttpMethod.GET, new HttpEntity(httpHeaders), ResponseMovie.class)
                 .getBody().getItems().stream()
